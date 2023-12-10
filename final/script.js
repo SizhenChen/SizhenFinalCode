2f3049f77279dfb9e3385d3b75a4d50d4123c385
SizhenChen <112890854+SizhenChen@users.noreply.github.com>
2023-12-10 16:47:17 +0800

    Add files via upload

---
+++ final/script.js
@@ -0,0 +1,62 @@
+let n = -1;
+
+var boxes = document.querySelectorAll('.Box');
+boxes.forEach(function (div, index) {
+    var boxId = 'box' + (index);
+    div.id = boxId;
+    // console.log(`${index}+${boxId}`)
+})
+
+var names = document.querySelectorAll('.name');
+names.forEach(function (h2, index) {
+    var nameId = 'name' + (index);
+    h2.id = nameId;
+    // console.log(`${index}+${nameId}`)
+})
+
+var addSigns = document.querySelectorAll('.add');
+addSigns.forEach(function (button) {
+    n++;
+    // console.log(`${num}`)
+    let Name = document.getElementById(`name${n}`).innerHTML;
+    // console.log(`${Name}`)
+    let Box = document.getElementById(`box${n}`);
+    button.addEventListener('click', function () {
+        let suggestion = prompt(`Please enter your suggestion for ${Name}`);
+        // console.log(suggestion)
+        let para = document.createElement("p");
+        para.style.margin = 0;
+        Box.appendChild(para);
+        para.innerText = `•${suggestion}`;
+
+        if (suggestion === null) {
+            para.innerText = null;
+        }
+    })
+})
+
+let m = -1;
+
+var hierarchies = document.querySelectorAll('.hierarchy');
+hierarchies.forEach(function (div, index) {
+    var hierarchyId = 'hierarchy' + (index);
+    div.id = hierarchyId;
+    // console.log(`${hierarchyId}`)
+})
+
+var showList = document.querySelectorAll('.showList');
+showList.forEach(function (button) {
+    m++;
+    let child = document.getElementById(`hierarchy${m + 1}`)
+    // console.log(`hierarchy${m + 1}`)
+    button.addEventListener('click', function () {
+        child.classList.toggle('show');
+
+        if (button.innerHTML === '▼') {
+            button.innerHTML = '▲';
+        } else if (button.innerHTML === '▲') {
+            button.innerHTML = '▼';
+        }
+    })
+})
+
